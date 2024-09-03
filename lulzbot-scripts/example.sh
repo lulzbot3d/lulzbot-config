729e5fc6730fd2f4dde5c831c013aa24e27d07a7
Carl Smith <csmith@lulzbot.com>
2024-09-03 14:55:59 -0500

    Pushing a meaningless change to test update manager setup.

--- lulzbot-scripts/example.sh
+++ lulzbot-scripts/example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# testing 123
+
 # A test to see if we can write to the log before klipper starts
 d=$(date)
 echo "Lulzbot Startup Script $d" >>/home/biqu/printer_data/logs/klippy.log
