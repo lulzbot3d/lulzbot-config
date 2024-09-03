70ea2a2002ec08e5e65eba1078c15182e45da527
Carl Smith <csmith@lulzbot.com>
2024-09-03 10:09:48 -0500

    Update refresh rate script.

--- lulzbot-scripts/set-refresh.sh
+++ lulzbot-scripts/set-refresh.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Add 40 and 35 Hz refresh modes for HDMI5 touchscreen, set to 40Hz.
 export DISPLAY=:0.0
 sudo xrandr --newmode "800x480_40.00" 19.50 800 824 896 992 480 483 493 496 -hsync +vsync
