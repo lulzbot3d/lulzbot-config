1a3d48f72ed6acae305c58dc94724f40dfd81678
Carl Smith <csmith@lulzbot.com>
2025-12-09 16:20:10 -0600

    Add update_beacon_id.sh script to startup script.

--- lulzbot-scripts/lulzbot-startup.sh
+++ lulzbot-scripts/lulzbot-startup.sh
@@ -2,4 +2,14 @@
 
 # Plans are, if we need to call a script when the system boots but before klipper starts, we will call it from here.
 
-# This functionality is not currently implemented on the AMOS, but may be in the future.
+# BEWARE - this script runs as user root, not as pi.  This can cause issues with file permissions that might
+# prevent your script from running properly.  So it might work better to put your script in another file and
+# call it from here with a command that runs it as user pi.
+# Something like:  sudo -u pi /home/pi/lulzbot-config/lulzbot-scripts/example.sh
+
+echo "Lulzbot Startup Script Start: $(date)" >>/home/pi/printer_data/logs/klippy.log
+
+# Automatically update the Beacon device path in printer.cfg at startup
+sudo -u pi /home/pi/lulzbot-config/lulzbot-scripts/update_beacon_id.sh
+
+echo "Lulzbot Startup Script End: $(date)" >>/home/pi/printer_data/logs/klippy.log
